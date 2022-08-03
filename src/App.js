95a1f4d9089c2b82791e700f5963ee2b65f73ac5
Olga O <42141921+olgamadej@users.noreply.github.com>
2022-08-03 17:28:46 +0200

    words json file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [words, setWords] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [wordsPerPage, setWordsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchWords = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://olgamadej.github.io/JSONs/french.json"
+      );
+      setWords(res.data);
+      setLoading(false);
+    };
+
+    fetchWords();
+  }, []);
+
+  console.log(words);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>The App</h1>
     </div>
   );
-}
+};
 
 export default App;
