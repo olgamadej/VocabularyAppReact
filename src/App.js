b97f067156f490cdc53e52dad8989fcaa85111dc
Olga O <42141921+olgamadej@users.noreply.github.com>
2022-08-04 09:57:01 +0200

    ''

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import React, { useState, useEffect } from "react";
-import Posts from "./components/Posts";
-import Pagination from "./components/Pagination";
+import Words from "./components/Words";
 import axios from "axios";
 import "./App.css";
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
+  const [words, setWords] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10);
+  const [wordsPerPage, setWordsPerPage] = useState(10);
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchWords = async () => {
       setLoading(true);
-      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      setPosts(res.data);
+      const res = await axios.get(
+        "https://olgamadej.github.io/JSONs/french_moving_around.json"
+      );
+
+      setWords(res.data);
       setLoading(false);
     };
 
-    fetchPosts();
+    fetchWords();
   }, []);
 
-  // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  console.log(words);
 
   return (
-    <div className="container mt-5">
-      <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts posts={currentPosts} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
-      />
+    <div className="container">
+      <h1 className="main-text my-3">Enjoy the Vocabulary</h1>
+      <Words words={words} loading={loading} />
     </div>
   );
 };
